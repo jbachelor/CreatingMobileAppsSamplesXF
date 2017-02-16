b887d5aad45fe21effbf1c8fc666fb13faecea4b
Jonathan Bachelor <Jonathan.Bachelor@hunterindustries.com>
2017-02-16 07:35:11 -0800

    Initial checkin - just a minimally functional prism forms app.

---
+++ CreatingMobileAppsSamples/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Prism.Unity;
+using CreatingMobileAppsSamples.Views;
+
+namespace CreatingMobileAppsSamples
+{
+	public partial class App : PrismApplication
+	{
+		public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+		protected override void OnInitialized()
+		{
+			InitializeComponent();
+
+			NavigationService.NavigateAsync(nameof(MainPage));
+		}
+
+		protected override void RegisterTypes()
+		{
+			Container.RegisterTypeForNavigation<MainPage>();
+		}
+	}
+}
+
---
+++ CreatingMobileAppsSamples/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+
+namespace CreatingMobileAppsSamples.ViewModels
+{
+	public class MainPageViewModel : BindableBase, INavigationAware
+	{
+		private string _title;
+		public string Title
+		{
+			get { return _title; }
+			set { SetProperty(ref _title, value); }
+		}
+
+		public MainPageViewModel()
+		{
+			Debug.WriteLine($"{this.GetType().Name}.{nameof(MainPageViewModel)}:  ctor");
+
+			Title = "I'm the main page!";
+		}
+
+		~MainPageViewModel()
+		{
+			Debug.WriteLine($"{this.GetType().Name}.{nameof(MainPageViewModel)}:  Destructor");
+		}
+
+		public void OnNavigatedFrom(NavigationParameters parameters)
+		{
+			Debug.WriteLine($"{this.GetType().Name}.{nameof(OnNavigatedFrom)}");
+		}
+
+		public void OnNavigatedTo(NavigationParameters parameters)
+		{
+			Debug.WriteLine($"{this.GetType().Name}.{nameof(OnNavigatedTo)}");
+		}
+	}
+}
+
